8bfe7c9a47bdd6f8314543abb79abdaf50a3c9b6
urvashi2004 <uy2204@gmail.com>
2024-09-30 23:41:25 +0530

    Added routing and basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import Faq from "./components/Faq/Faq"
-import Home from "./components/Home/Home"
-import Login from "./components/Login/Login"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from "./components/Home/Home";
+import Relax from "./components/Relax/Relax";
+import Resources from "./components/Resources/Resources";
+import Faq from "./components/Faq/Faq";
+import Navbar from "./components/Home/Navbar/Navbar";
 
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      <Home />
-      <Faq />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mindfulness" element={<Relax />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/faq" element={<Faq />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Relax/Relax.jsx
+++ src/components/Relax/Relax.jsx
@@ -1,9 +1,22 @@
-import "./Relax.css"
+import "./Relax.css";
 
 const Relax = () => {
   return (
-    <div>Relax</div>
-  )
-}
+    <div className="relax-page">
+      <div className="container container-1">
+        <h2>BreatheEasy</h2>
+        <p>Master the art of controlled breathing to calm your mind and body.</p>
+      </div>
+      <div className="container container-2">
+        <h2>ZenMaster</h2>
+        <p>Engage in guided meditation sessions that lead you to inner peace.</p>
+      </div>
+      <div className="container container-3">
+        <h2>ChillPill</h2>
+        <p>Practical tips and techniques to help you relax and unwind quickly.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Relax
+export default Relax;
