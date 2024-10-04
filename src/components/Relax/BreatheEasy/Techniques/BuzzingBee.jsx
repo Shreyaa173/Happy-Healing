d0ad91f19889999a12d4ed14c5b119d6fc1563cf
Shreya <shreya186btcse23@igdtuw.ac.in>
2024-10-04 22:29:17 +0530

    some changes in path

--- src/components/Relax/BreatheEasy/Techniques/BuzzingBee.jsx
+++ src/components/Relax/BreatheEasy/Techniques/BuzzingBee.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BreathingLayout from './BreathingLayout';
+import BreathingLayout from './BreathingLayout/BreathingLayout';
 
 const BuzzingBumblebee = () => {
   const name = "Buzzing Bumblebee Breathing";
