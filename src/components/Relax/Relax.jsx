bee70d8722f11e50b9391dbb8368a759be282f3b
urvashi2004 <uy2204@gmail.com>
2024-10-05 09:50:25 +0530

    relax page done

--- src/components/Relax/Relax.jsx
+++ src/components/Relax/Relax.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import "./Relax.css";
-import Image from './Images/Logo.jpg';
+import BreathingLady from './Images/BreathingLady.jpeg';
+import ZenImage from './Images/ZenImage.jpg'
 
 const Relax = () => {
   return (
     <div className="relax-page">
 
       <Link to="/mindfulness/breathe-easy" className="containers">
-        <img src={Image} alt="Breathing Techniques" className="containers-image" />
+        <img src={BreathingLady} alt="Breathing Techniques" className="containers-image" />
         <div className="content">
           <h2>BreatheEasy</h2>
           <p>Master the art of controlled breathing to calm your mind and body.</p>
@@ -17,18 +18,10 @@ const Relax = () => {
       </Link>
       
       <Link to="/mindfulness/zen-master" className="containers">
-        <img src={Image} alt="ZenMaster" className="containers-image" />
+        <img src={ZenImage} alt="ZenMaster" className="containers-image" />
         <div className="content">
           <h2>ZenMaster</h2>
           <p>Engage in guided meditation sessions that lead you to inner peace.</p>
-        </div>
-      </Link>
-
-      <Link to="/mindfulness/chill-pill" className="containers">
-        <img src={Image} alt="ChillPill" className="containers-image" />
-        <div className="content">
-          <h2>ChillPill</h2>
-          <p>Practical tips and techniques to help you relax and unwind quickly.</p>
         </div>
       </Link>
 
